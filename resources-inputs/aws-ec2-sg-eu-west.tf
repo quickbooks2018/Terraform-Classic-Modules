ba0c43047a40718d1aaa41fbd9d0d57e3b173fc7
Asim Arain <chasim1982@gmail.com>
2018-12-12 18:40:02 +0500

    resource inputs removed

--- resources-inputs/aws-ec2-sg-eu-west.tf
+++
@@ -1,9 +0,0 @@
-module "cloudelligent-ec2-sg" {
-  source = "./modules/ec2-sg"
-  region = "eu-west-2"
-  vpc-id = "${module.cloudelligent-vpc.vpc-id}"
-  ec2-sg-name = "ec2-sg"
-
-
-
-}
--- resources-inputs/aws-vgw-eu-west.tf
+++
@@ -1,6 +0,0 @@
-module "cloudelligent-vgw" {
-  source = "./modules/vgw"
-  region = "eu-west-2"
-  vpc-id = "${module.cloudelligent-vpc.vpc-id}"
-  vpn-gateway-name = "Cloudelligent-vgw"
-}

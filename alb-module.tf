3cfb854d2bc5d4ddab393cf1be50a3cb8898ec72
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2019-02-11 15:58:07 +0500

    alb module with ACM

--- alb-module.tf
+++ alb-module.tf
@@ -7,7 +7,7 @@ module "alb" {
   alb-sg = "${module.cloudelligent-ec2-sg.ec2-sg-security-group}"
   alb-subnets = "${module.vpc.public-subnet-ids}"
   alb-tag = "cloudelligent-alb"
-  certificate-arn = "arn:aws:acm:us-east-1:898668804275:certificate/fef70c69-2971-4456-9020-4e35d9f8f742"
+  certificate-arn = "arn:aws:acm:us-east-1:898668804275:certificate/fef70c69-2971-4456-9020-4e35d9f8f74244585544"
 
   #Target Groups
 
